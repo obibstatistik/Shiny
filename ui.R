c0a5a3e1bc1513c2a5cf488f64754bd6ed85ca61
Kent Gottschalk Hansen <kentgottschalk@gmail.com>
2017-03-16 15:24:49 +0100

    initial commit

---
+++ ui.R
@@ -0,0 +1,51 @@
+library(foreach)
+library(shiny)
+library(shinydashboard)
+library(shinyjs)
+
+dirs <- list.dirs(path = ".", recursive=FALSE)
+
+dashboardPage(
+  skin = "blue",
+  
+  dashboardHeader(
+    title = "Statistik Obib"
+  ),
+  
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Statistik Dashboards", tabName = "dashboards", icon = icon("bar-chart", lib="font-awesome")),
+      menuItem("Kilder", tabName = "kilder", icon = icon("file-text-o", lib="font-awesome")),
+      menuItem("Datasikkerhed", tabName = "datasikkerhed", icon = icon("file-text-o", lib="font-awesome")),
+      menuItem("Dokumentation", tabName = "dokumentation", icon = icon("file-text-o", lib="font-awesome")
+      )
+    )
+  ),
+  
+  dashboardBody(
+    tabItems(
+    
+      tabItem(tabName = "dashboards",
+        fixedPage(column(12,
+            foreach(i=dirs) %do% box(width = 6, includeMarkdown(paste0((gsub(".*/", "", i)),"/readme.md")))
+          )
+        )
+      ),
+      tabItem(tabName = "kilder",
+        box(width = 12,
+          includeMarkdown("kilder.md")
+        )
+      ),
+      tabItem(tabName = "datasikkerhed",
+        box(width = 12,
+          includeMarkdown("datasikkerhed.md")
+        )
+      ),
+      tabItem(tabName = "dokumentation",
+        box(width = 12,
+          includeMarkdown("dokumentation.md")
+        )
+      )
+    )
+  )
+)
